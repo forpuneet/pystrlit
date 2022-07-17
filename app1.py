3170d8b7c8a7b581b98efe282e89b537aecdb157
forpuneet <46962918+forpuneet@users.noreply.github.com>
2022-07-17 12:14:50 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,11 +2,11 @@ import streamlit as st
 PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
 #st.beta_set_page_config(**PAGE_CONFIG)
 def main():
-	st.title("Awesome Streamlit for ML")
-	st.subheader("How to run streamlit from colab")
-	menu = ["Home","About"]
+	st.title("Testing for Module 5 Mini Project")
+	st.subheader("Yeah... trying Mini project")
+	menu = ["Airline Tweet Analysis","About"]
 	choice = st.sidebar.selectbox('Menu',menu)
 	if choice == 'Home':
-		st.subheader("Streamlit From Colab")	
+		st.subheader("Tweet Sentiment Analysis")	
 if __name__ == '__main__':
 	main()
