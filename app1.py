0caa61d92a0b38d85d918687aac81a039b01a3fa
forpuneet <46962918+forpuneet@users.noreply.github.com>
2022-08-29 22:58:45 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,9 +2,9 @@ import streamlit as st
 PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
 #st.beta_set_page_config(**PAGE_CONFIG)
 def main():
-	st.title("Testing for Module 5 Mini Project")
-	st.subheader("Yeah... trying Mini project")
-	menu = ["Airline Tweet Analysis","About"]
+	st.title("Gigmo Solutions - Remote Identity Verification")
+	s#t.subheader("Yeah... trying Mini project")
+	menu = ["Remote Identity Verification","onboarding"]
 	choice = st.sidebar.selectbox('Menu',menu)
 	if choice == 'Home':
 		st.subheader("Tweet Sentiment Analysis")	
