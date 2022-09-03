afc2d991f5574c2709ab3a85c219a0f7dd23ead6
forpuneet <46962918+forpuneet@users.noreply.github.com>
2022-09-03 18:36:17 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import cv2
+import numpy as np
+
+
 PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
 #st.beta_set_page_config(**PAGE_CONFIG)
 def main():
@@ -11,5 +15,23 @@ def main():
 		picture = st.camera_input("Take a picture")
 		if picture:
      			st.image(picture)
+	
+	
+	
+	cap = cv2.VideoCapture(1)
+
+	currentFrame = 0
+	while(True):
+    		print(currentFrame)
+   
+    		ret, frame = cap.read()
+		frame = cv2.flip(frame,1)
+		cv2.imshow('frame',frame)
+    		if cv2.waitKey(1) & 0xFF == ord('q'):
+        		break
+		currentFrame += 1
+cap.release()
+cv2.destroyAllWindows()
+	
 if __name__ == '__main__':
 	main()
